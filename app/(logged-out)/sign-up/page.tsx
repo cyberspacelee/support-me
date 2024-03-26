3d19e20938e6089098128444333874818f3cc8e2
cyberspacelee <xingcanli132@163.com>
2024-03-27 00:15:27 +0800

    add sign up email & account type input

--- app/(logged-out)/sign-up/page.tsx
+++ app/(logged-out)/sign-up/page.tsx
@@ -1,5 +1,37 @@
+"use client";
 import React from 'react'
 import { z } from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
+import { CalendarIcon, PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 
 const formSchema = z
     .object({
@@ -72,8 +104,80 @@ const formSchema = z
 
 
 const SignUp = () => {
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+            passwordConfirm: "",
+            companyName: "",
+        },
+    });
+
+    const accountType = form.watch("accountType");
+
+    const handleSubmit = (data: z.infer<typeof formSchema>) => {
+        console.log("login validation passed: ", data);
+        router.push("/dashboard");
+    };
+
+    const dobFromDate = new Date();
+    dobFromDate.setFullYear(dobFromDate.getFullYear() - 120);
+
     return (
-        <div>SignUp</div>
+        <>
+            <PersonStandingIcon size={50} />
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle>Sign up</CardTitle>
+                    <CardDescription>Sign up for a new SupportMe account</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form
+                            className="flex flex-col gap-4"
+                            onSubmit={form.handleSubmit(handleSubmit)}
+                        >
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="john@doe.com" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                                          <FormField
+                control={form.control}
+                name="accountType"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Account type</FormLabel>
+                    <Select onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select an account type" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="personal">Personal</SelectItem>
+                        <SelectItem value="company">Company</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+                        </form></Form>
+                </CardContent>
+            </Card>
+        </>
     )
 }
 
